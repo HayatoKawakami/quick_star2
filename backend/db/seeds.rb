2577a55df76e700802b7d31a535e6f9abb37c0f1
HayatoKawakami <redbull.drip.mother@gmail.com>
2022-09-28 07:37:24 +0900

    backendファイルを初期化しセットアップまで行う

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,11 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-50.times do |n|
-  Post.create!(
-    title: "テスト#{n + 1}",
-    content: "これは単なる#{n + 1}です"
-  )
-end
