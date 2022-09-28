1731a6b2f52f1d131bcb70c8fd579a27a81372d0
HayatoKawakami <redbull.drip.mother@gmail.com>
2022-09-28 21:53:56 +0900

    frontendとbackendの連携コーディング中

---
+++ frontend/src/component/Container.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {UserShow} from "./contents/users/show";
+
+class Container extends React.Component {
+  render(){
+    return( 
+      <div className="container">
+        <h1>これはcontainerです！！</h1>
+        <UserShow />
+      </div>
+    );
+  } 
+
+}
+
+export { Container };
