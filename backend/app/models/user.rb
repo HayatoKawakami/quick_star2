0f0b000c80f93bf42be3c71d005533632f4001b6
HayatoKawakami <redbull.drip.mother@gmail.com>
2022-10-11 10:35:46 +0900

    アカウント新規作成機能を実装完了

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
-  validates :password_confirmation, presence: true
   validates :sex, presence: true
   validates :birthday, presence: true
 
