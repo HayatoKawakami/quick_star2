aad40690b932247241e99f373159800e6ac76df9
HayatoKawakami <redbull.drip.mother@gmail.com>
2022-10-24 21:35:16 +0900

    ログイン機能の実装中

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :password, presence: true
   validates :sex, presence: true
   validates :birthday, presence: true
   # validates :image, presence: true
 
-  has_secure_password
   # 画像アップロードの準備
   mount_uploader :image, ImageUploader
 end
