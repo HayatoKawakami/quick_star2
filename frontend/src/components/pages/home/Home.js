27a5d999455d79d7abd1410c78e9cb568c2dc799
HayatoKawakami <redbull.drip.mother@gmail.com>
2022-11-10 09:37:46 +0900

    ホーム画面を実装完了

--- frontend/src/components/pages/home/Home.js
+++ frontend/src/components/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useConstContext } from "../../../contexts/ConstContext";
 import { useLoggedInStatusContext } from "../../../contexts/LoginContext";
 import { useItemContext } from "../../../contexts/ItemContext";
@@ -8,14 +8,30 @@ import { Link } from "react-router-dom";
 export const Home = () => {
 
   const { items } = useItemContext();
-  const { logged_in } = useLoggedInStatusContext();
+  const { logged_in, user } = useLoggedInStatusContext();
+
+  const [date, setDate] = useState('');
+
+  const getDate = () => {
+    const dateObj = new Date();
+    const aryWeek = ["月","火","水","木","金","土","日"]
+
+    const dateText = dateObj.getFullYear() + "年" +
+                      dateObj.getMonth() + "月" +
+                      dateObj.getDate() + "日" +
+                      " " + aryWeek[dateObj.getDay()] + "曜日"
+
+    setDate(dateText);
+  }
 
   useEffect(() => {
-  })
+    getDate();
+  },[date])
 
   return(
     <div>
-      <h1>Home</h1>
+      <h1>{user.name} さん、こんにちは。</h1>
+      <p>{date}</p>
       <h3>Target</h3>
       <ul>
       {Object.values(items).map((item, index) => {
