5b39586490bb9558f18f3bc372df2f2bbf9d8c77
HayatoKawakami <redbull.drip.mother@gmail.com>
2022-12-16 22:50:36 +0900

    フロントエンドのテストをコーディング中

---
+++ frontend/src/__tests__/UserNew.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import { UserNew } from '../components/pages/users/new';
+
+test("ユーザー新規登録画面に「hayato」が正常に表示されている", ()=>{
+  render(<UserNew/>);
+  const hayato = screen.getByText("hayato");
+  expect(hayato).toBeInTheDocument();
+})
