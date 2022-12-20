1647b4989ee1824b40b5e231477d2f88b1a01734
HayatoKawakami <redbull.drip.mother@gmail.com>
2022-12-20 23:09:12 +0900

    テストコーディング中

--- frontend/src/__tests__/UserNew.test.js
+++ frontend/src/__tests__/UserNew.test.js
@@ -1,9 +1,17 @@
-
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import { UserNew } from '../components/pages/users/new';
+import userEvent from "@testing-library/user-event";
 
 test("ユーザー新規登録画面に「hayato」が正常に表示されている", ()=>{
   render(<UserNew/>)
   const hayato = screen.getByText("hayato");
   expect(hayato).toBeInTheDocument();
+})
+
+test("ユーザー新規登録画面に新規登録ボタンが表示されている", () => {
+  render(<UserNew/>)
+  const text = screen.getByText("新規登録")
+  expect(text).toBeInTheDocument()
 })
